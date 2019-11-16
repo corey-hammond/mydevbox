db62303e7739efc49381e263f13472b03ee04a32
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-11-16 12:51:39 -0800

    configured admin

--- toolbox/admin.py
+++ toolbox/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import ToolBox, Tool
 
-# Register your models here.
+admin.site.site_header = "MyDevBox Admin"
+admin.site.site_title = "MyDevBox Admin Area"
+admin.site.index_title = "Welcome to the MyDevBox Admin Area"
+
+
+class ToolInLine(admin.TabularInline):
+    model = Tool
+    extra = 3
+
+
+class ToolBoxAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["language"]}),
+    ]
+    inlines = [ToolInLine]
+
+    list_display = ("language",)
+
+    list_filter = ["language"]
+
+    search_fields = ["language"]
+
+
+admin.site.register(ToolBox, ToolBoxAdmin)
+admin.site.register(Tool)
--- toolbox/models.py
+++ toolbox/models.py
@@ -1,4 +1,7 @@
+import datetime
+from django.utils import timezone
 from django.db import models
+
 
 class ToolBox(models.Model):
     language = models.CharField(max_length=100)
@@ -6,11 +9,17 @@ class ToolBox(models.Model):
     def __str__(self):
         return self.language
 
+    class Meta:
+        verbose_name = "Tool Box"
+        verbose_name_plural = "Tool Boxes"
+
+
 class Tool(models.Model):
-    toolbox = models.ForeignKey(ToolBox, on_delete=models.CASCADE, related_name='tools')
+    toolbox = models.ForeignKey(ToolBox, on_delete=models.CASCADE, related_name="tools")
     title = models.CharField(max_length=100)
     tool_type = models.CharField(max_length=100)
     content = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title 
+        return self.title
